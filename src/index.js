6d72c99992a309e79c098b43ff6e8d5c1f5d3499
Jevin <therealjevin@gmail.com>
2023-01-15 19:55:00 -0600

    completed minimum deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const tasksList = document.querySelector('#tasks');
+    const li = document.createElement('li');
+    const formText = document.getElementById('new-task-description').value;
+    li.textContent = formText;
+    tasksList.append(li);
+  })
 });
