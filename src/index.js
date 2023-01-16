2947acb7ef5e7552033dfdd72a1bc7e132474d45
Jevin <therealjevin@gmail.com>
2023-01-15 20:55:08 -0600

    added priority option and associated color coding

--- src/index.js
+++ src/index.js
@@ -7,6 +7,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const li = document.createElement('li');
     const formText = document.getElementById('new-task-description').value;
     li.textContent = formText;
+    const selection = document.getElementById('priority');
+    const priority = selection.options[selection.selectedIndex];
+
+
+    switch(priority.value) {
+      case 'high':
+        li.className = 'high-prio';
+        break;
+      case 'medium':
+        li.className = 'med-prio';
+        break;
+      case 'low':
+        li.className = 'low-prio';
+        break;
+    }
+    console.log(priority);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'X';
+    li.append(deleteButton);
+    deleteButton.addEventListener('click', e => {
+      li.remove();
+    });
     tasksList.append(li);
-  })
+  });
 });
